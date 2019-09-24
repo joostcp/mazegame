374318de33e4dd5869868a38517bae5dde2f9129
joost <joost.polderman@first8.nl>
2019-09-24 15:59:27 +0200

    een poging de friends en foes random te laten bewegen. werk nog niet. wel een deelnemer-class gemaakt, waar alle deelnemers van erven

---
+++ game_logic/src/main/java/org/joost/games/mazegame/entities/Deelnemer.java
@@ -0,0 +1,23 @@
+package org.joost.games.mazegame.entities;
+
+public abstract class Deelnemer implements Moveable {
+
+    Tile tile;
+    int damage;
+    int health;
+
+
+    abstract String getChar();
+
+    public int getHealth() {
+        return health;
+    }
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
+    public boolean move( Direction direction ) {
+        return false;
+    }
+}
